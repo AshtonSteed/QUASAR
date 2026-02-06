1e57890bb8b8ee87b51e406c65457e764cfcb629
Ashton Steed <glen53simos53@gmail.com>
2026-02-06 16:57:48 -0500

    Added Motive connection driver, added logging callbacks and basic printing

--- GSCore/drivers/cf_client.py
+++ GSCore/drivers/cf_client.py
@@ -1,9 +1,14 @@
 # Might not need, cflib wrappers for common stuff
 # Sending 6DOF to UAV, changing setpoint, UAV parameters, logging functions
+import queue
 import time
 import threading
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
+from GSCore.drivers.motive_client import start_motive_stream
+from GSCore.drivers.mock_motive_client import start_mock_stream
+from common_classes import SystemState, Pose
 
 class CrazyflieDriver:
     def __init__(self, uri, pose_queue, command_queue):
@@ -60,6 +65,7 @@ class CrazyflieDriver:
             return
 
         self.running = True
+        self.start_position_logging() # Start logging position data from CF
         self.control_thread = threading.Thread(target=self._control_loop, daemon=True)
         self.control_thread.start()
         print("Control loop started.")
@@ -91,7 +97,108 @@ class CrazyflieDriver:
 
     def _connection_lost_cb(self, link_uri, msg):
         print(f"Connection Lost: {msg}")
+        
+    # Kalman Filter Arming
+    
+    def _arm_kalman(self, err=1e-3):
+        """
+        Enables Kalman Filter on crazyflie, allowing for external pose updates from Motive.
+        """
+        # Enable EKF and set position error 
+        print("Configuring EKF parameters...")
+        # Use Kalman state estimator
+        self.cf.param.set_value('stabilizer.estimator', '2') # EKF
+        # Set the expected position error from Motive [mm]
+        self.cf.param.set_value('locSrv.extPosStdDev', err)
+        # Set the expected attitude error from Motive [degrees]
+        self.cf.param.set_value('locSrv.extQuatStdDev', 5 * err)
+        
+        pose = self._extract_pose_from_queue()
+        if pose is None:
+            print("No valid pose data available to arm Kalman filter.")
+            return
+        
+        x, y, z = pose.x, pose.y, pose.z
+        
+        
+        self.cf.param.set_value('kalman.initialX', x)
+        self.cf.param.set_value('kalman.initialY', y)
+        self.cf.param.set_value('kalman.initialZ', z)
+        
+        self.cf.param.set_value('kalman.resetEstimation', '1')
+        
+        time.sleep(0.5) # Wait for CF to start up EKF
+        
+        self.cf.param.set_value('kalman.resetEstimation', '0')
+        
+        print("EKF armed with initial position from Motive.")
+        
+    def _extract_pose_from_queue(self):
+        """
+        Non-blocking check for new pose data from Motive. Returns None if no new data.
+        """
+        try:
+            # Try to get most recent pose stored from motive
+            packet = self.pose_queue.get_nowait()
+            if packet.valid:
+                # If tracking is valid, return the pose data
+                return packet
+            else:
+                pass
+        except queue.Empty:  
+            pass
+        # If invalid or motive isnt sending, return None
+        return None
 
+    def _send_state(self):
+        # Send latest pose data if it exists, otherwise do nothing
+        pose = self._extract_pose_from_queue()
+        if pose is None:
+            # Invalid position, do not update state estimation or commands
+            return
+        mx, my, mz, qx, qy, qz, qw = pose.x, pose.y, pose.z, pose.qx, pose.qy, pose.qz, pose.qw
+        self.cf.extpos.send_extpose(mx, my, mz, qx, qy, qz, qw)
+        
+    from cflib.crazyflie.log import LogConfig
+
+    def start_position_logging(self):
+        # 1. Configure the Log Block
+        # Period is how often the crazyflie sends data
+        log_conf = LogConfig(name='Position', period_in_ms=50)
+
+      
+        # Add all variables to log from CF parameter list
+        log_conf.add_variable('stateEstimate.x', 'float')
+        log_conf.add_variable('stateEstimate.y', 'float')
+        log_conf.add_variable('stateEstimate.z', 'float')
+        log_conf.add_variable('pm.batteryLevel')
+        
+
+        # 3. Add the configuration to the Crazyflie
+        try:
+            self.cf.log.add_config(log_conf)
+            
+            # 4. Attach a callback function (What happens when data arrives?)
+            log_conf.data_received_cb.add_callback(self.position_data_callback)
+            
+            # 5. Start the logging
+            log_conf.start()
+            print("Logging started!")
+            
+        except KeyError as e:
+            print(f"Could not find variable in TOC: {e}")
+        except AttributeError:
+            print("Could not add log config, bad configuration.")
+    
+    # Function ran whenever log data is received from CF
+    def position_data_callback(self, timestamp, data, logconf):
+        x = data['stateEstimate.x']
+        y = data['stateEstimate.y']
+        z = data['stateEstimate.z']
+        bat = data['pm.batteryLevel']
+    
+        # Example: Print only occasionally to avoid spamming console
+        print(f"[{timestamp}] Pos: ({x:.2f}, {y:.2f}, {z:.2f}) | Battery: {bat}%")
     # ==========================================
     # THE CONTROL WORKER
     # ==========================================
@@ -100,4 +207,25 @@ class CrazyflieDriver:
         """
         The main flight loop. Runs in background thread.
         """
+        self._arm_kalman()
         
+        while self.running:
+            self._send_state()
+            time.sleep(0.1) # 10Hz update rate
+        
+        
+        
+
+def test_cf_connection(uri):
+    pose_queue = queue.Queue(maxsize=1)
+    shared_state = SystemState()
+    motive_client = start_mock_stream(pose_queue, shared_state)
+    cflib.crtp.init_drivers()
+    driver = CrazyflieDriver(uri, pose_queue, None)
+    if driver.connect():
+        print("Connection successful!")
+        driver.start()
+        time.sleep(10)  # Let it run for a bit
+        driver.stop()
+    else:
+        print("Connection failed.")
--- main.py
+++ main.py
@@ -1,27 +1,16 @@
 # Entry point of program
 import queue
-import queue
 import threading
 import time
 from GSCore.drivers.motive_client import print_motive_data, start_motive_stream, start_print_thread
 from GSCore.drivers.mock_motive_client import print_mock_data, start_mock_stream
 from common_classes import SystemState, Pose
 from GSCore.data.logger import logger_worker
+from GSCore.drivers.cf_client import test_cf_connection
 
 def main():
-    stop_event = threading.Event()
-    pose_queue = queue.Queue(maxsize=1)
-    shared_state = SystemState()
-    motive_client = start_motive_stream(pose_queue, shared_state)
-
-    # Start logging thread
-    logger_thread = threading.Thread(target=logger_worker, args=(shared_state, stop_event, ))
-    logger_thread.start()
-    start_print_thread(pose_queue)
-    time.sleep(30)  # Let it log for 10 seconds
-    # Stop logging
-    stop_event.set()
-    logger_thread.join()
+    test_cf_connection("radio://0/80/2M") # Test Crazyflie connection with mock data stream
+    
 
 if __name__ == "__main__":
     main()
