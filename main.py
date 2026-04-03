b5cbc8f0559f94cf123c201caad2ee93bac7b44c
Ashton Steed <glen53simos53@gmail.com>
2026-04-03 14:59:23 -0400

    fixed cf uris, matched to crazyflie uavs

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def start_testbed():
 
     # 1. Define Swarm Configuration
     swarm_config = [
-        {"id": "CF_1", "uri": "radio://0/110/2M/E7E7E7E701", "motive_id": 1},
-        {"id": "CF_2", "uri": "radio://0/110/2M/E7E7E7E702", "motive_id": 2},
-        {"id": "CF_3", "uri": "radio://0/110/2M/E7E7E7E703", "motive_id": 3},
+        {"id": "CF_1", "uri": "radio://0/110/1M/E7E7E7E701", "motive_id": 1},
+        {"id": "CF_2", "uri": "radio://0/110/1M/E7E7E7E702", "motive_id": 2},
+        {"id": "CF_3", "uri": "radio://0/110/1M/E7E7E7E703", "motive_id": 3},
     ]
     
     # 2. Instantiate Agents
